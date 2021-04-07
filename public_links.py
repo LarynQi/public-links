71bc16088e25d3b5ed725687a50af2ccd04563fd
Laryn Qi <larynqi@berkeley.edu>
2021-04-07 02:27:17 -0700

    auto-refresh

--- public_links.py
+++ public_links.py
@@ -42,6 +42,9 @@ def go(shortlink):
             return redirect(links[shortlink])
         return make_response('Link missing. Please follow the instructions on the spreadsheet.', 403)
     else:
+        refresh()
+        if shortlink in links:
+            return go(shortlink)
         return make_response('Link not found', 404)
 
 @app.route('/preview/<path:shortlink>')
@@ -49,4 +52,7 @@ def preview(shortlink):
     if shortlink in links:
         return f'<div> Points to <a href="{links[shortlink]}">{links[shortlink]}</a></div> <div> Created by {authors[shortlink] if authors[shortlink] else "N/A"} on {dates[str(shortlink)] if dates[str(shortlink)] else "N/A"} </div>'
     else:
+        refresh()
+        if shortlink in links:
+            return preview(shortlink)
         return make_response('Link not found', 404)
